b2e38968166d20cd1991d9c010fa94c9cd0cc60f
Dhinesh <testing.iddhi@gmail.com>
2024-09-24 12:35:27 +0530

    update on 24/09/24

--- src/test/java/Razorpay/dummy.java
+++ src/test/java/Razorpay/dummy.java
@@ -1,7 +1,6 @@
 package Razorpay;
 
 import java.time.Duration;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -11,27 +10,34 @@ import utility.Components;
 
 public class dummy {
 
-    static WebDriver driver;
+        public static void main(String[] args) throws InterruptedException {
+                WebDriver driver = Components.startChrome(true);
+                WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(120));
+                driver.get("https://www.google.com/recaptcha/api2/demo");
+                driver.manage().window().maximize();
+                wait.until(ExpectedConditions
+                                .frameToBeAvailableAndSwitchToIt(By.xpath("//iframe[@title='reCAPTCHA']")));
+                wait.until(ExpectedConditions
+                                .visibilityOf(driver.findElement(By.xpath("//label[text()=\"I'm not a robot\"]"))))
+                                .click();
+                driver.switchTo().defaultContent();
+                captchaBypasser(driver, wait);
+                Thread.sleep(5000);
+                wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//input[@type=\"submit\"]"))))
+                                .click();
+        }
 
-    public static void main(String[] args) throws InterruptedException {
-        driver = Components.startChrome();
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(120));
-        driver.get("https://www.google.com/recaptcha/api2/demo");
-        driver.manage().window().maximize();
-        wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.xpath("//iframe[@title='reCAPTCHA']")));
-        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//label[text()=\"I'm not a robot\"]"))))
-                .click();
-        driver.switchTo().defaultContent();
-        wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(
-                By.xpath("//iframe[@title=\"recaptcha challenge expires in two minutes\"]")));
-        System.out.println("Frame switched....");
-        Thread.sleep(1500);
-        wait.until(ExpectedConditions
-                .visibilityOf(driver.findElement(By.xpath("//div[@class='button-holder help-button-holder']"))))
-                .click();
+        public static void captchaBypasser(WebDriver driver, WebDriverWait wait) throws InterruptedException {
+                wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(
+                                By.xpath("//iframe[@title=\"recaptcha challenge expires in two minutes\"]")));
+                System.out.println("Frame switched....");
+                Thread.sleep(1500);
+                wait.until(ExpectedConditions
+                                .visibilityOf(driver.findElement(
+                                                By.xpath("//div[@class='button-holder help-button-holder']"))))
+                                .click();
 
-        driver.switchTo().defaultContent();
-        Thread.sleep(5000);
-        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//input[@type=\"submit\"]")))).click();
-    }
+                driver.switchTo().defaultContent();
+        }
+
 }
